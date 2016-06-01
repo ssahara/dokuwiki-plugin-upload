839ca9c8e7c41ada4efce998ad2b8f84bf9e4ba9
Satoshi Sahara <sahara.satoshi@gmail.com>
2016-06-01 20:00:21 +0900

    prevent unnecessary "upload fail" message

--- action.php
+++ action.php
@@ -8,25 +8,8 @@
  */
 
 if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
-require_once DOKU_PLUGIN . 'action.php';
-require_once(DOKU_INC . 'inc/media.php');
-require_once(DOKU_INC . 'inc/infoutils.php');
 
 class action_plugin_upload extends DokuWiki_Action_Plugin {
-
-    function getInfo() {
-        return array(
-            'author' => 'Franz Häfner',
-            'email' => 'fhaefner@informatik.tu-cottbus.de',
-            'date' => '2010-09-07',
-            'name' => 'upload plugin',
-            'desc' => 'upload plugin can add a link to the media manager in your wikipage.
-            			Basic syntax: {{upload>namespace|option1|option2}}
-                Use @page@ as namespage to use ID of the actual page as namespace or @current@ to use the namespace the current page is in.',
-            'url' => 'https://www.dokuwiki.org/plugin:upload',
-        );
-    }
 
     /**
      * Register its handlers with the DokuWiki's event controller
@@ -37,7 +20,7 @@ class action_plugin_upload extends DokuWiki_Action_Plugin {
     }
 
     function _hook_function_cache(&$event, $param) {
-        if($_FILES['upload']['tmp_name']) {
+        if ($_FILES['upload']['tmp_name']) {
             $event->preventDefault();
             $event->stopPropagation();
             $event->result = false;
@@ -51,15 +34,16 @@ class action_plugin_upload extends DokuWiki_Action_Plugin {
     }
 
     function _hook_function_upload(&$event, $param) {
-        global $lang;
-        global $INPUT;
+        global $INPUT, $lang;
+
         // get namespace to display (either direct or from deletion order)
-        $NS = $_POST['ns'];
+        $NS = $INPUT->post->('ns');
+        if (!$NS) return;
         $NS = cleanID($NS);
 
         // check auth
         $AUTH = auth_quickaclcheck("$NS:*");
-        if($AUTH < AUTH_UPLOAD) {
+        if (($AUTH < AUTH_UPLOAD) && $_FILES['upload']['tmp_name']) {
             msg($lang['uploadfail'], -1);
             return;
         }
